c771105cf3e08b0f0387a53c18936533fd2c87d1
Kufok <151053473+Kufok@users.noreply.github.com>
2024-01-25 16:16:01 +0100

    update lol

--- lol.py
+++ lol.py
@@ -11,40 +11,40 @@ if user_input == ("A"):
     print("A = Ano")
     print("B = Ne")
     user_input_jeskyne = input()
-   
+    
     if user_input_jeskyne == ("A"):
         print("Sel jsi za zvukem kde jsi uvidel lva ktery te zabil")
         print("Prohral si")
-   
+    
     if user_input_jeskyne ==("B"):
         print("Jsi unaveny pujdes spat?")
         print("A = Ano")
         print("B = Ne")
         user_input_spanek = input()
-       
+
         if user_input_spanek == ("A"):
             print("Ve spanku te zabil lev ktereho jsi slysel")
             print("Prohral si")
-       
+     
         if user_input_spanek == ("B"):
             print("V noci jses bal ale kdyz jsi uvidel lva tak jsi utekl a zachranil jses")
             print("Vyhral si")
 
-if user_input == ("B"):
+elif user_input == ("B"):
     print("Vydal jses po strasidelne ceste kde jsi narazil na bazinu co udelas?")
     print("A = Vratis se")
     print("B = Zkusis jit do baziny a preplavat to")
     print("C = Vytahnes hulku a postavis most")
     user_input_cesta = input()
-    
+     
     if user_input_cesta == ("A"):
-        print("Kdyz jses varcel tak te napadli banditi a zabili te")
-        print("Prohral si")
-    
+            print("Kdyz jses varcel tak te napadli banditi a zabili te")
+            print("Prohral si")
+     
     if user_input_cesta == ("B"):
-        print("V bazine byl jozin z bazin a zabil te")
-        print("Prohral si")
-    
+         print("V bazine byl jozin z bazin a zabil te")
+         print("Prohral si")
+     
     if user_input_cesta == ("C"):
         print("Postavil si most pres ktery jsi presel cestou jsi podkal pani ktera prodavala koste za jeden zlatak. Koupis ho?")
         print("A = Ano")
@@ -52,15 +52,15 @@ if user_input == ("B"):
         user_input_koste = input()
         
         if user_input_koste == ("A"):
-            print("Odletl si")
-            print("Vyhral si")
-       
+             print("Odletl si")
+             print("Vyhral si")
+    
         if user_input_koste == ("B"):
             print("Prosel si kolem ale cestou te prepadli banditi a zabili te")
             print("Prohral si")
 
-if user_input == ("C"):
-    print("Sel jsi za svetelem vydel jsi dum zatukal si na dvere a stara pani ti otevrela ty si vesel dovnitr a ptala se te jesstli neco nechces?")
+elif user_input == ("C"):
+    print("Sel jsi za svetelem, vydel jsi dum, zatukal si na dvere a stara pani ti otevrela, ty si vesel dovnitr a ptala se te jesstli neco nechces?")
     print("A = Ano")
     print("B = Ne")
     user_input_svetlo = input()
@@ -68,6 +68,22 @@ if user_input == ("C"):
     if user_input_svetlo == ("A"):
         print("Pani ti priesla jidlo ty si ochutnal ale jidlo bylo otravene a umrel jsi")
         print("Prohral jsi")
-    
+        
     if user_input_svetlo == ("B"):
         print("Pani ti rekla aby si prespal. Prespis?")
+        print("A = Ano")
+        print("B = Ne")
+        user_input_perespat = input()
+
+        if user_input_perespat == ("A"):
+            print("Pani te zabila ve spnaku")
+            print("Prohral jsi")
+
+        if user_input_perespat == ("B"):
+            print("Prezil jsi")
+            print("Vyhral jsi")
+
+else:
+    print("Takova moznost neexistuje")
+
+    
